e0fddb3bccfec901c67a1b6bd886ec67bdf55c95
amemem <ameme698@student.liu.se>
2021-10-10 14:38:24 +0200

    parallel:: knapsack_brute_force and Rcpp:: knapsack_dynamic

--- R/knapsack_brute_force.R
+++ R/knapsack_brute_force.R
@@ -1,4 +1,4 @@
-knapsack_brute_force = function(x, W) {
+knapsack_brute_force = function(x, W, parallel = FALSE) {
   stopifnot(
     is.data.frame(x),
     colnames(x) %in% c("v", "w"),
@@ -9,13 +9,34 @@ knapsack_brute_force = function(x, W) {
   n = length(x[["v"]])
   value = 0
   elements = c()
-  for (i in 1:(2^n-1)) {
-    index = as.integer(intToBits(i)) * 1:32
-    if (sum(x[index, "w"]) <= W) {
-      v = sum(x[index, "v"])
-      if (v > value) {
+  if (!parallel) {
+    for (i in 1:(2^n-1)) {
+      index = as.integer(intToBits(i)) * 1:32
+      if (sum(x[index, "w"]) <= W) {
+        v = sum(x[index, "v"])
+        if (v > value) {
+          value = v
+          elements = index[index > 0]
+        }
+      }
+    }
+  }
+  else {
+    cores = parallel::detectCores()
+    cl = parallel::makeCluster(cores, "PSOCK")
+    parL = parallel::parLapply(cl, 1:(2^n-1), function(i) {
+      index = as.integer(intToBits(i)) * 1:32
+      if (sum(x[index, "w"]) <= W) {
+        return(list("value" = sum(x[index, "v"]),
+                    "elements" = index[index > 0]))
+      }
+    })
+    parallel::stopCluster(cl)
+    for (i in 1:length(parL)) {
+      v = parL[[i]]$value
+      if (!is.null(v) && v > value) {
         value = v
-        elements = index[index > 0]
+        elements = parL[[i]]$elements
       }
     }
   }
--- R/knapsack_dynamic.R
+++ R/knapsack_dynamic.R
@@ -1,4 +1,4 @@
-knapsack_dynamic = function(x, W) {
+knapsack_dynamic = function(x, W, fast = FALSE) {
   stopifnot(
     is.data.frame(x),
     colnames(x) %in% c("v", "w"),
@@ -8,15 +8,37 @@ knapsack_dynamic = function(x, W) {
   )
   n = length(x[["v"]])
   m = matrix(0, n + 1, W + 1)
-  for (i in 1:n) {
-    for (j in 1:W) {
-      if (x[i, "w"] <= j) {
-        m[i+1, j+1] = max(m[i, j+1-x[i, "w"]] + x[i, "v"], m[i, j+1])
-      }
-      else {
-        m[i+1, j+1] = m[i, j+1]
+  if (!fast) {
+    for (i in 1:n) {
+      for (j in 1:W) {
+        if (x[i, "w"] <= j) {
+          m[i+1, j+1] = max(m[i, j+1-x[i, "w"]] + x[i, "v"], m[i, j+1])
+        }
+        else {
+          m[i+1, j+1] = m[i, j+1]
+        }
       }
     }
+  }
+  else {
+    Rcpp::cppFunction('NumericMatrix dp(int n, int W,
+                                        NumericVector w,
+                                        NumericVector v)
+    {
+      NumericMatrix m(n+1, W+1);
+      for (int i = 1; i <= n; ++i) {
+        for (int j = 1; j <= W; ++j) {
+          if (w[i-1] <= j) {
+            m(i, j) = std::max(m(i-1, j-w[i-1]) + v[i-1], m(i-1, j));
+          }
+          else {
+            m(i, j) = m(i-1, j);
+          }
+        }
+      }
+      return m;
+    }')
+    m = dp(n, W, x$w, x$v)
   }
   elements = vector("numeric", n)
   value = m[n+1, W+1]
